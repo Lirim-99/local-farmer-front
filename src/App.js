df44ff2d4bf70311916e1ef915ef2369a076eed4
Blend Halili <53181578+Lirim-99@users.noreply.github.com>
2023-10-20 17:27:28 +0200

    Made changes in the view and login register part, also reorganised  the project folders and files

--- src/App.js
+++ src/App.js
@@ -1,61 +1,12 @@
-import React from 'react';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import ProductManagementRouter from './productManagement/productManagementRouter'; // Import the Product Management Router
 
-import Login from './components/login.component';
-import SignUp from './components/signup.component';
-import UserDetails from './components/user-details';
-import ProductForm from './productManagement/productForm/product-form';
 
-function App() {
-  const isLoggedIn = window.localStorage.getItem("loggedIn");
+import React from 'react'
+import NavBar from './components/NavBar'
+
+const App = () => {
   return (
-    <Router>
-      <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-          <div className="container">
-            <Link className="navbar-brand" to={'/sign-in'}>
-              Local Farmer
-            </Link>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to={'/sign-in'}>
-                    Login
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={'/sign-up'}>
-                    Sign up
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={'/product-form'}>
-                    Product Form
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-
-        <div className="auth-wrapper">
-          <div className="auth-inner">
-            <Routes>
-              <Route exact path="/" element={isLoggedIn === "true" ? <UserDetails /> : <Login />} />
-              <Route path="/sign-in" element={<Login />} />
-              <Route path="/sign-up" element={<SignUp />} />
-              <Route path="/userDetails" element={<UserDetails />} />
-              <Route path="/product-form" element={<ProductForm />} />
-              <Route path="/product-management" element={<ProductManagementRouter />} /> {/* Use 'element' to render components */}
-            </Routes>
-          </div>
-        </div>
-      </div>
-    </Router>
-  );
+   <NavBar></NavBar>
+  )
 }
 
-export default App;
+export default App
